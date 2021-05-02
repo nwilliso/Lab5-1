8f4baf9da30384c813321469423a20c338472cf0
Nicholas Willison <nwilliso@ucsd.edu>
2021-05-01 22:59:55 -0700

    functioning meme generator

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
 // script.js
 
 const img = new Image(); // used to load image from <input> and draw to canvas
+const file = document.getElementById('image-input') // file select
+const canvas = document.getElementById('user-image'); // canvas
+const ctx = canvas.getContext('2d'); // for drawing
+const generate = document.querySelector("[type='submit']") // generate button
+const clear = document.querySelector("[type='reset']") // generate button
+const read = document.querySelector("[type='button']") // generate button
+const top = document.getElementById('text-top'); // top text input
+const bottom = document.getElementById('text-bottom'); // bottom text input
+const synth = window.speechSynthesis; // for speaking
+const voice = document.getElementById('voice-selection'); // voice selection
+const volume = document.getElementById('volume-group'); // volume group
+const slider = document.querySelector("[type='range']"); // actual slider
+const icon = document.getElementsByTagName('img')[0];
+var voices = [];
+
+// from Mozilla (https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis)
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for(var i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voice.appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+voice.remove(0);
+voice.disabled = false;
+// end from Mozilla
+
 
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
-  // TODO
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  generate.disabled = false;
+  clear.disabled = true;
+  read.disabled = true;
+
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const dim = getDimensions(canvas.width, canvas.height, img.width, img.height);
+  ctx.drawImage(img, dim.startX, dim.startY, dim.width, dim.height);
+
+
 
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
@@ -23,7 +75,7 @@ img.addEventListener('load', () => {
  * and also the starting X and starting Y coordinate to be used when you draw the new image to the
  * Canvas. These coordinates align with the top left of the image.
  */
-function getDimmensions(canvasWidth, canvasHeight, imageWidth, imageHeight) {
+function getDimensions(canvasWidth, canvasHeight, imageWidth, imageHeight) {
   let aspectRatio, height, width, startX, startY;
 
   // Get the aspect ratio, used so the picture always fits inside the canvas
@@ -51,3 +103,63 @@ function getDimmensions(canvasWidth, canvasHeight, imageWidth, imageHeight) {
 
   return { 'width': width, 'height': height, 'startX': startX, 'startY': startY }
 }
+
+// loading image input
+file.addEventListener('change', () => {
+  //event.preventDefault();
+  img.src = URL.createObjectURL(file.files[0]);
+  img.alt = file.files[0].name;
+});
+
+// submitting text
+const form = document.getElementById('generate-meme')
+form.addEventListener('submit', (event) => {
+  ctx.fillStyle = 'white';
+  ctx.font = '75px Impact'
+  ctx.textAlign = 'center'
+  ctx.fillText(top.value, canvas.width/2, 75);
+  ctx.fillText(bottom.value, canvas.width/2, 375);
+  event.preventDefault();
+
+  generate.disabled = true;
+  clear.disabled = false;
+  read.disabled = false;
+});
+
+// clear canvas and form
+clear.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  generate.disabled = false;
+  clear.disabled = true;
+  read.disabled = true;
+});
+
+// read text
+read.addEventListener('click', () => {
+  let speech = new SpeechSynthesisUtterance(top.value + ' ' + bottom.value);
+  
+  // from Mozilla (https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis)
+  let sound = voice.selectedOptions[0].getAttribute('data-name');
+  for(var i = 0; i < voices.length ; i++) {
+    if(voices[i].name === sound) {
+      speech.voice = voices[i];
+    }
+  }
+  speech.volume = slider.value/100;
+  speechSynthesis.speak(speech);
+  //end from Mozilla
+
+});
+
+// adjust volume
+volume.addEventListener('input', () => {
+  if (slider.value >= 67) {
+    icon.src = 'icons/volume-level-3.svg';
+  } else if (slider.value >= 34) {
+    icon.src = 'icons/volume-level-2.svg';
+  } else if (slider.value >= 1) {
+    icon.src = 'icons/volume-level-1.svg';
+  } else {
+    icon.src = 'icons/volume-level-0.svg';
+  }
+});
